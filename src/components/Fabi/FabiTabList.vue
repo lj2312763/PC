16b3235605cd3a515fd52f3abe5c574fb442eef7
lj2312763@163.com <lijian2312763>
2020-03-30 12:04:49 +0800

    优化币种类型选择

--- src/components/Fabi/FabiTabList.vue
+++ src/components/Fabi/FabiTabList.vue
@@ -1,52 +1,97 @@
 <template>
-  <el-tabs type="border-card" v-model="activeName" @tab-click="changeTab" class="a">
-    <el-tab-pane
-      v-for="(item, i) in currency"
-      :label="item.name"
-      :name="item.name"
-      :key="i"><List :unit="item.name" :type="type"/></el-tab-pane>
-  </el-tabs>
+  <div>
+    <div class="custom-tabs-container">
+      <ul v-model="activeName" class="custom-tabs">
+        <li
+          :class="activeName === item.name ? 'active' : ''"
+          v-for="(item, i) in currency"
+          :label="item.name"
+          :name="item.name"
+          @click="changeTab(item)"
+          :key="i"><div>{{item.name}}</div>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <List :unit="activeName" :type="type"/>
+    </div>
+  </div>
+
 </template>
 
 <script>
-  import List from './List'
-  const list  = [
-    {name: 'USDT'},
-    {name: 'BTC'},
-    {name: 'ETH'},
-    {name: 'EX900'},
-    {name: 'EOS'},
-    {name: 'NKC'},
-    {name: 'BCH'},
-  ]
-    export default {
-      name: "fabi-tab-list",
-      props: [ 'type'],
-      components: {
-        List
-      },
-      data() {
-        return {
-          currency: list,
-          activeName: list[0].name
-        };
-      },
-      mounted() {
+    import List from './List'
 
-      },
-      computed: {},
-      methods: {
-        changeTab(tab, event) {
-          console.log(tab.name, event);
-          this.activeName = tab.name
+    const list = [
+        {name: 'USDT'},
+        {name: 'BTC'},
+        {name: 'ETH'},
+        {name: 'EX900'},
+        {name: 'EOS'},
+        {name: 'NKC'},
+        {name: 'BCH1'},
+        {name: 'BCH2'},
+        {name: 'BCH3'},
+    ]
+    export default {
+        name: "fabi-tab-list",
+        props: ['type'],
+        components: {
+            List
+        },
+        data() {
+            return {
+                currency: list,
+                item: {},
+                activeName: list[0].name
+            };
+        },
+        mounted() {
+
+        },
+        computed: {},
+        methods: {
+            changeTab(item) {
+                this.activeName = item.name
+            }
         }
-      }
     };
 </script>
 
-<style scoped lang="less">
-  .el-tabs--border-card>.el-tabs__content{
-    padding: 0;
+<style lang="less">
+  @color2: #1c69b9;
+  @color3: #DCDFE6;
+  @item-height: 3rem;
+  .custom-tabs-container{
+    height: @item-height;
+    overflow: hidden;
+    padding: 0 9px;
+  }
+  .custom-tabs {
+    display: flex;
+    overflow-x: scroll;
+    height: 4.1rem;
+    li {
+      width: 5rem;
+      height: @item-height;
+      padding: 4px 6px;
+      cursor: pointer;
+      padding-bottom: 0;
+      border-bottom: 1px solid @color3;
+      div{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+      }
+    }
+    li.active{
+      color: @color2;
+      div{
+        border-bottom: 2px solid @color2;
+      }
+    }
   }
 
 </style>
